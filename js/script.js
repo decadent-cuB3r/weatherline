ab19e8f44e553ac0a25c57d278df1ff2440932c0
decadent-cuB3r <71422011+decadent-cuB3r@users.noreply.github.com>
2020-12-15 20:37:41 +0800

    .nav_menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+$(document).ready(function() {
+    $(".nav_menu").click(function() {
+        $('#nav_list').toggleClass("open")
+    });
+});
+
+
 function changeAirPollution() {
     document.getElementById('air_pollution').innerHTML = document.getElementById('districts').value
 }
-$(document).ready(function() {
-
-});
